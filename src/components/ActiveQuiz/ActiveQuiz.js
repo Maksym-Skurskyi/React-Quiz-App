cbf499000d809bf3a8a16203a4c978b820a0c175
skerski <qmaksim.maksimenko@gmail.com>
2021-05-30 18:35:55 +0300

    Start

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,21 @@
+import classes from "./ActiveQuiz.module.css"
+import React from 'react'
+const ActiveQuiz = () => {
+	return <div className={classes.ActiveQuiz}>
+		<p>
+			<span>
+				<strong>2.</strong>
+				how r u
+			</span>
+		</p>
+
+		<ul>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+			<li>4</li>
+		</ul>
+	</div>
+}
+
+export default ActiveQuiz
