ced3d6c68bf727c84b5c787917e3fee0d8d37139
skerski <qmaksim.maksimenko@gmail.com>
2021-07-06 09:11:17 +0300

    Api path updated

--- src/api/quiz.js
+++ src/api/quiz.js
@@ -3,5 +3,5 @@ import axios from "axios"
 const { REACT_APP_DATABASE_URL } = process.env
 
 export default axios.create({
-	baseURL: REACT_APP_DATABASE_URL+'/',
+	baseURL: REACT_APP_DATABASE_URL,
 })
