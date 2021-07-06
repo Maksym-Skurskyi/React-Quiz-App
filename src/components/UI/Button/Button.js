9576ff3da73ae1459b9a56fbdb20ac89ec19919d
skerski <qmaksim.maksimenko@gmail.com>
2021-07-06 13:37:04 +0300

    Changed button

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -8,7 +8,7 @@ const Button = (props) => {
 		<button
 			onClick={props.onClick}
 			className={cls.join(' ')}
-			// disabled={props.disabled}
+			disabled={props.disabled}
 		>
 			{props.children}
 		</button>
